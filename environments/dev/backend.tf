0498fa3c155bca832ce33d5c58b1ee614ec777af
Your Name <you@example.com>
2025-12-25 13:49:40 +0530

    Configure Azure Storage backend tfstatemirhakim01 for Terraform state

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-tf-backend"
-    storage_account_name = "tfterraformstate01"
+    storage_account_name = "tfstatemirhakim01"
     container_name       = "tfstate"
     key                  = "dev.terraform.tfstate"
   }
 }
+
