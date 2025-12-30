3250fa47c82ec30f7315e7b2f8dd776216dd6a14
Your Name <you@example.com>
2025-12-30 16:15:44 +0530

    Add terraform backend configuration for dev

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-tf-backend"
-    storage_account_name = "tfstatemirhakim01"
+    storage_account_name = "tfbackend1767090006"
     container_name       = "tfstate"
     key                  = "dev.terraform.tfstate"
   }
